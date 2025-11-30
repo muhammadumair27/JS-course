2cadb459216467f31d219e615071f8b4459ef3bd
Muhammad Umair <urafique539@gmail.com>
2025-11-30 15:25:19 +0000

    strings learning

---
+++ 01_basics/strings.js
@@ -0,0 +1,9 @@
+// *********** TODAY WE LEARN ABOUT STRINGS IN DETAIL AND ITS METHODS **********
+
+let name= "Umair"    // aik method to yeh hy string ko declare krny ka
+let age=23;
+//now we are going to concatenate both of the strings that is called ((**STRING INTERPOLATION**))
+console.log(`My name is ${name}. And my age is ${age}.`); 
+
+let new_name=new String ('Muhammmad-umair');    // string declare krny ka yeh aik aur new tareeqa hy, iss mn value ((** KEY PAIR VALUE**)) form mn bhi mil jati hy
+console.log(new_name);
